01f960a8275624b3afa68ca0c6566e66a8e72d25
Márcio Tenório <marciotenorioj@gmail.com>
2022-11-26 17:07:14 -0300

    Import project

---
+++ src/test/java/br/imd/distprog/procurement/ProcurementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.distprog.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
